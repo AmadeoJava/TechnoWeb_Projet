10a23fbbc32016f3b0719803ae88eaf665650d8a
AmadeoJava <78850142+AmadeoJava@users.noreply.github.com>
2022-04-17 13:39:32 +0000

    Add files via upload
    Table v1

--- technoweb/src/AdminPage.js
+++ technoweb/src/AdminPage.js
@@ -27,19 +27,20 @@ import CreateIcon from '@mui/icons-material/Create';
 import BorderAllIcon from '@mui/icons-material/BorderAll';
 import BarChartIcon from '@mui/icons-material/BarChart';
 import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
 import ArticleIcon from '@mui/icons-material/Article';
 import $ from "jquery";
 import TextField from '@mui/material/TextField';
 import FormControl from '@mui/material/FormControl';
 import TextareaAutosize from '@mui/base/TextareaAutosize';
 import Grid from '@mui/material/Grid';
-import Select from '@mui/material/Select';
-import InputLabel from '@mui/material/InputLabel';
-import Link from '@mui/material/Link';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import TablePagination from '@mui/material/TablePagination';
 import './adminpage.css';
 
 const drawerWidth = 240;
@@ -155,7 +156,91 @@ const StyledMenu = styled((props) => (
 
 const icones=[<CreateIcon/>, <BorderAllIcon/>, <BarChartIcon/>];
 
+
+interface Column {
+  id: 'name' | 'code' | 'population' | 'size' | 'density';
+  label: string;
+  minWidth?: number;
+  align?: 'right';
+  format?: (value: number) => string;
+}
+
+const columns:  Column[] = [
+  { id: 'name', label: 'Name', minWidth: 170 },
+  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  {
+    id: 'population',
+    label: 'Population',
+    minWidth: 170,
+    align: 'right',
+    format: (value: number) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'size',
+    label: 'Size\u00a0(km\u00b2)',
+    minWidth: 170,
+    align: 'right',
+    format: (value: number) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'density',
+    label: 'Density',
+    minWidth: 170,
+    align: 'right',
+    format: (value: number) => value.toFixed(2),
+  },
+];
+
+interface Data {
+  name: string;
+  code: string;
+  population: number;
+  size: number;
+  density: number;
+}
+
+function createData(
+  name: string,
+  code: string,
+  population: number,
+  size: number,
+): Data {
+  const density = population / size;
+  return { name, code, population, size, density };
+}
+
+const rows = [
+  createData('India', 'IN', 1324171354, 3287263),
+  createData('China', 'CN', 1403500365, 9596961),
+  createData('Italy', 'IT', 60483973, 301340),
+  createData('United States', 'US', 327167434, 9833520),
+  createData('Canada', 'CA', 37602103, 9984670),
+  createData('Australia', 'AU', 25475400, 7692024),
+  createData('Germany', 'DE', 83019200, 357578),
+  createData('Ireland', 'IE', 4857000, 70273),
+  createData('Mexico', 'MX', 126577691, 1972550),
+  createData('Japan', 'JP', 126317000, 377973),
+  createData('France', 'FR', 67022000, 640679),
+  createData('United Kingdom', 'GB', 67545757, 242495),
+  createData('Russia', 'RU', 146793744, 17098246),
+  createData('Nigeria', 'NG', 200962417, 923768),
+  createData('Brazil', 'BR', 210147125, 8515767),
+];
+
 export default function WebProject() {
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
   const listeBox = ["principal", "editer", "table", "graphe", "doc"];
@@ -192,23 +277,39 @@ export default function WebProject() {
 
   const inputButtons = ["userinputbutton", "placeinputbutton", "eventinputbutton"];
   const inputForms = ["userform", "placeform", "eventform"];
-  var selected=0;
+  var selectedi=0;
   const inputCard = (el) =>{
     for (let i=0 ;i<inputButtons.length; i++){
       $("#"+inputButtons[i]).css("color","#0e8cd4");
       $("#"+inputButtons[i]).css("background-color","#fff");
       if(inputButtons[i]==el){
-        selected=i;
+        selectedi=i;
       }
     }
     $("#"+el).css("background-color","#0e8cd4");
     $("#"+el).css("color","#fff");
-    cardchosed(inputForms[selected]);
+    cardchosed(inputForms,inputForms[selectedi]);
   }
 
-  const cardchosed = (el) =>{
-    for(let i=0; i<inputForms.length; i++){
-      $("#"+inputForms[i]).hide();
+  const tableButtons = ["usertablebutton", "placetablebutton", "eventtablebutton"];
+  const tableForms = ["usertable", "placetable", "eventtable"];
+  var selectedt=0;
+  const tableCard = (el) =>{
+    for (let i=0 ;i<tableButtons.length; i++){
+      $("#"+tableButtons[i]).css("color","#0e8cd4");
+      $("#"+tableButtons[i]).css("background-color","#fff");
+      if(tableButtons[i]==el){
+        selectedt=i;
+      }
+    }
+    $("#"+el).css("background-color","#0e8cd4");
+    $("#"+el).css("color","#fff");
+    cardchosed(tableForms,tableForms[selectedt]);
+  }
+
+  const cardchosed = (tab,el) =>{
+    for(let i=0; i<tab.length; i++){
+      $("#"+tab[i]).hide();
     }
     $("#"+el).show();
   }
@@ -220,7 +321,8 @@ export default function WebProject() {
 
   window.onload = (function() {
     fermer("principal");
-    inputCard(inputButtons[selected]);
+    inputCard(inputButtons[selectedi]);
+    tableCard(tableButtons[selectedt]);
     cardchosed(inputForms[0]);
     $('#placeupload').change(function(e){
       var fs=e.target.files.length;  // filesize
@@ -511,6 +613,75 @@ export default function WebProject() {
         <DrawerHeader />
           <Card className='centerDiv'>
             <h2>Tables</h2>
+            <nav className='liste'>
+              <Button onClick={event=>tableCard("usertablebutton")} id="usertablebutton" style={{width: "50%"}} sx={{ mt: 3, mb: 2 }}>
+                Utilisateurs
+              </Button>
+              <Button onClick={event=>tableCard("placetablebutton")} id="placetablebutton" style={{width: "50%"}} sx={{ mt: 3, mb: 2 }}>
+                Lieux
+              </Button>
+              <Button onClick={event=>tableCard("eventtablebutton")} id="eventtablebutton" style={{width: "50%"}} sx={{ mt: 3, mb: 2 }}>
+                Evènements
+              </Button>
+            </nav>
+
+            <Box id="usertable" component="table" noValidate sx={{ mt: 1 }} style={{width: "98%"}}>
+              <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+                <TableContainer sx={{ maxHeight: 600 }}>
+                  <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                      <TableRow>
+                        {columns.map((column) => (
+                          <TableCell
+                            key={column.id}
+                            align={column.align}
+                            style={{ minWidth: column.minWidth }}
+                          >
+                            <strong>{column.label}</strong>
+                          </TableCell>
+                        ))}
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                        return (
+                          <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                            {columns.map((column) => {
+                              const value = row[column.id];
+                              return (
+                                  <TableCell key={column.id} align={column.align}>
+                                    {column.format && typeof value === 'number'
+                                      ? column.format(value)
+                                      : value}
+                                  </TableCell>
+                                );
+                              })}
+                            </TableRow>
+                          );
+                        })}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+                <TablePagination
+                  rowsPerPageOptions={[10, 25, 100]}
+                  component="div"
+                  count={rows.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+              </Paper>
+            </Box>
+
+            <Box id="placetable" component="table" noValidate sx={{ mt: 1 }}>
+              <h1>place</h1>
+            </Box>
+
+            <Box id="eventtable" component="table" noValidate sx={{ mt: 1 }}>
+              <h1>event</h1>
+            </Box>
+
           </Card>
       </Box>
 
