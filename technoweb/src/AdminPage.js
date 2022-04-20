4c54a01465b7b761fd6e3cded3e81b237fa3589e
AmadeoJava <78850142+AmadeoJava@users.noreply.github.com>
2022-04-20 13:47:30 +0000

    Add files via upload
    Tableau V1

--- technoweb/src/AdminPage.js
+++ technoweb/src/AdminPage.js
@@ -156,75 +156,34 @@ const StyledMenu = styled((props) => (
 
 const icones=[<CreateIcon/>, <BorderAllIcon/>, <BarChartIcon/>];
 
+function createDataUsers(firstname, name, surname, administrateur, password) {
+  return { firstname, name, surname, administrateur, password};
+}// add data
+const rowsUsers = [
+  createDataUsers('Sherlock', 'Holmes', 'Détective', "Oui", "********"),
+  createDataUsers('Shinishi', 'Kudo', 'Lycéen', "Non", "********"),
+  createDataUsers('Jules', 'Maigret', 'Commisaire', "Non", "********"),
+  createDataUsers('David', 'Panzoli', 'Dieu ?', "Non", "********"),
+  createDataUsers('Naruto', 'Uzumaki', 'Hokage', "Non", "********"),
+  createDataUsers('Monkey', 'Luffy', 'Pirate', "Non", "********"),
+  createDataUsers('Natsu', 'Dragnir', 'Salamander', "Non", "********"),
+  createDataUsers('Arnaud', 'Cros', 'Ark Tryharder', "Oui", "********"),
+  createDataUsers('Antonin', 'Pidet', 'Fourmi', "Oui", "********"),
+  createDataUsers('Amadéo', 'Soufflet', 'Pentester', "Oui", "********"),
+  createDataUsers('François', 'Pouit', 'PuitPuit', "Non", "********"),
+  createDataUsers('Nicolas', 'Garric', 'Bricoleur', "Non", "********"),
+  createDataUsers('Laura', 'Brillon', 'Matheuse', "Non", "********"),
+  createDataUsers('Thierry', 'Montaut', 'Relou', "Non", "********"),
 
-interface Column {
-  id: 'name' | 'code' | 'population' | 'size' | 'density';
-  label: string;
-  minWidth?: number;
-  align?: 'right';
-  format?: (value: number) => string;
-}
-
-const columns:  Column[] = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-  {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toFixed(2),
-  },
 ];
 
-interface Data {
-  name: string;
-  code: string;
-  population: number;
-  size: number;
-  density: number;
-}
 
-function createData(
-  name: string,
-  code: string,
-  population: number,
-  size: number,
-): Data {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
+const columnsUsers = [
+  { id: 'firstname'},
+  { id: 'name'},
+  { id: 'surname'},
+  { id: 'administrateur'},
+  { id: 'password'}
 ];
 
 export default function WebProject() {
@@ -232,11 +191,11 @@ export default function WebProject() {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
-  const handleChangePage = (event: unknown, newPage: number) => {
+  const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
 
-  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
@@ -627,35 +586,37 @@ export default function WebProject() {
 
             <Box id="usertable" component="table" noValidate sx={{ mt: 1 }} style={{width: "98%"}}>
               <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-                <TableContainer sx={{ maxHeight: 600 }}>
-                  <Table stickyHeader aria-label="sticky table">
+                <TableContainer sx={{ maxHeight: 440 }}>
+                  <Table stickyHeader aria-label="sticky table">  
                     <TableHead>
                       <TableRow>
-                        {columns.map((column) => (
-                          <TableCell
-                            key={column.id}
-                            align={column.align}
-                            style={{ minWidth: column.minWidth }}
-                          >
-                            <strong>{column.label}</strong>
-                          </TableCell>
-                        ))}
+                        <TableCell align="center" style={{fontWeight: "bold"}}>Prénom</TableCell>
+                        <TableCell align="center" style={{fontWeight: "bold"}}>Nom</TableCell>
+                        <TableCell align="center" style={{fontWeight: "bold"}}>Pseudo</TableCell>
+                        <TableCell align="center" style={{fontWeight: "bold"}}>Administrateur</TableCell>
+                        <TableCell align="center" style={{fontWeight: "bold"}}>Mot de passe</TableCell>
+                        <TableCell align="center" style={{fontWeight: "bold"}}>Modifier</TableCell>
                       </TableRow>
                     </TableHead>
                     <TableBody>
-                        {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                        return (
-                          <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                            {columns.map((column) => {
-                              const value = row[column.id];
-                              return (
-                                  <TableCell key={column.id} align={column.align}>
-                                    {column.format && typeof value === 'number'
-                                      ? column.format(value)
-                                      : value}
+                      {rowsUsers
+                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        .map((row) => {
+                          return (
+                            <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                              {columnsUsers.map((column) => {
+                                const value = row[column.id];
+                                return (
+                                  <TableCell key={column.id} align="center">
+                                    {value}
                                   </TableCell>
                                 );
                               })}
+                              <TableCell align="center">
+                                <IconButton id={row[columnsUsers[2].id]} onClick={event=>alert(row[columnsUsers[2].id])}>
+                                  <EditIcon style={{color: '#1976d2'}}/>
+                                </IconButton>
+                              </TableCell>
                             </TableRow>
                           );
                         })}
@@ -663,14 +624,14 @@ export default function WebProject() {
                   </Table>
                 </TableContainer>
                 <TablePagination
-                  rowsPerPageOptions={[10, 25, 100]}
-                  component="div"
-                  count={rows.length}
-                  rowsPerPage={rowsPerPage}
-                  page={page}
-                  onPageChange={handleChangePage}
-                  onRowsPerPageChange={handleChangeRowsPerPage}
-                />
+        rowsPerPageOptions={[5, 10, 25, 100]}
+        component="div"
+        count={rowsUsers.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
               </Paper>
             </Box>
 
