067c02506cf4bda89454e3aa7b76ebca748c9049
Antonin81 <Antonin.pidet@gmail.com>
2022-04-29 16:20:08 +0200

    Modifications de l'accueil et création d'un premier footer

---
+++ technoweb/src/FooterComponent.js
@@ -0,0 +1,74 @@
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+
+export default function FooterComponent() {
+  return (
+    <footer>
+      <Box
+        px={{ xs: 3, sm: 10 }}
+        py={{ xs: 5, sm: 10 }}
+        bgcolor="text.secondary"
+        color="white"
+      >
+        <Container maxWidth="lg">
+          <Box style={{display:'flex', textAlign:'center'}}>
+              <Box style={{ marginLeft:'33%', paddingRight:'10%'}}>
+              <Box borderBottom={1}>Help</Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Contact
+                </Link>
+              </Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Support
+                </Link>
+              </Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Privacy
+                </Link>
+              </Box>
+            </Box>
+            <Box style={{paddingRight:'10%'}}>
+              <Box borderBottom={1}>Account</Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Login
+                </Link>
+              </Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Register
+                </Link>
+              </Box>
+            </Box>
+            <Box>
+              <Box borderBottom={1} style={{paddingRight:'10%'}}>Messages</Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Backup
+                </Link>
+              </Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  History
+                </Link>
+              </Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Roll
+                </Link>
+              </Box>
+            </Box>
+          </Box>
+          <Box textAlign="center" pt={{ xs: 5, sm: 10 }} pb={{ xs: 5, sm: 0 }}>
+            Nous, les Créateurs Absolus &reg; {new Date().getFullYear()}
+          </Box>
+        </Container>
+      </Box>
+    </footer>
+  );
+}
--- technoweb/src/Home.js
+++ technoweb/src/Home.js
@@ -1,17 +1,99 @@
-import { Link } from "react-router-dom";
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import Button from '@mui/material/Button';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import './home.css';
+import $ from "jquery";
 import MapComponent from "./MapComponent";
-import './index.css';
+import { Link } from "react-router-dom";
+import FooterComponent from "./FooterComponent";
+
+//import './index.css';
+
+
+const drawerWidth = 240;
+
+
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+
+
 
 function Home() {
-  return <div>
-    <Link to="/admin"> Link to admin dashboard </Link>
-    <MapComponent />
-    <br></br>
-    <Link to="/form"> Link to formulaire </Link>
-    <br></br>
-    <Link to="/login"> Link to login </Link>
 
-  </div>;
+
+  const fonction = () =>{
+    $('#event').remove();
+  };
+
+  return(<Box>
+  <Box position='relative'> 
+    <CssBaseline />
+    <AppBar id="barreTop" position='relative'>
+    <Toolbar>
+      <div id='presentation'>
+      <img alt='logo' id='logo' src={require('./images/logo/logo.png')}/>
+      <Typography id='Albivouac'>Albivouac</Typography>
+      </div>
+      <div id='boutons'>
+      <Link to="/form" class="lienAccueil">
+        <Button >
+        Questionnaire
+        </Button>
+      </Link>
+      <Link to="/login" class="lienAccueil">
+        <Button >
+          Connexion
+        </Button>
+      </Link>   
+      <Link to="/admin" class="lienAccueil">
+        <Button >
+          Dashboard
+        </Button>
+      </Link>  
+      </div>
+    </Toolbar>
+  </AppBar>
+
+  </Box>
+  <Card sx={{ display: 'flex', width:'100%' }} id="event" >
+    <Typography sx={{ flexGrow: 1 }}>
+    Ya un event
+    </Typography>
+    <HighlightOffIcon onClick={fonction}/>
+  </Card>
+  <div id="mapAccueil" >
+  <MapComponent/>
+  </div>
+
+  <Card style={{position:'realative', bottom:'0', width:'100%', left:'0'}}>
+  <FooterComponent/>
+  </Card>
+  </Box>
+  );
   
 }
 
